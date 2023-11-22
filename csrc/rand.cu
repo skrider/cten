1dcf39cf6045e94f0e6ee02c3d26d3517f355d8c
skrider <skrider@berkeley.edu>
2023-11-22 23:19:12 +0000

    randu working

--- csrc/rand.cu
+++ csrc/rand.cu
@@ -73,3 +73,53 @@ __global__ void randn_kernel(Tensor<float, 2> t, curandStateMtgp32_t *states,
 template void randn(Tensor<float, 2> t, float shift, float scale, long seed);
 template Tensor<float, 2> randn(shape_t<2> t, float shift, float scale,
                                 long seed);
+
+template <uint D> void randu(Tensor<int, D> t, int a, int b, long seed) {
+  // use 256 threads total as this is maximum supported by curand mtgp32
+  dim3 threadsPerBlock(256);
+  // use max 200 blocks as mtgp supports generating up to 200 states
+  dim3 numBlocks(MIN(200, t.size / threadsPerBlock.x));
+
+  mtgp32_kernel_params_t *kernel_params;
+  curandStateMtgp32_t *states;
+  curandStatus_t s;
+
+  checkCudaErrors(cudaMalloc(&kernel_params, sizeof(mtgp32_kernel_params_t)));
+  checkCudaErrors(
+      cudaMalloc(&states, sizeof(curandStateMtgp32_t) * numBlocks.x));
+
+  s = curandMakeMTGP32Constants(mtgp32dc_params_fast_11213, kernel_params);
+  if (s != CURAND_STATUS_SUCCESS) {
+    printf("curandMakeMTGP32Constants failed with status %d\n", s);
+    exit(1);
+  }
+
+  // make one state for each block
+  s = curandMakeMTGP32KernelState(states, mtgp32dc_params_fast_11213,
+                                  kernel_params, numBlocks.x, seed);
+  if (s != CURAND_STATUS_SUCCESS) {
+    printf("curandMakeMTGP32KernelState failed with status %d\n", s);
+    exit(1);
+  }
+  randu_kernel<<<numBlocks, threadsPerBlock>>>(t, states, a, b);
+  checkCudaErrors(cudaDeviceSynchronize());
+  checkCudaErrors(cudaFree(kernel_params));
+  checkCudaErrors(cudaFree(states));
+}
+
+__global__ void randu_kernel(Tensor<int, 2> t, curandStateMtgp32_t *states,
+                             int a, int b) {
+  // load state to shared memory
+  __shared__ curandStateMtgp32_t state;
+  state = states[blockIdx.x];
+  // grid stride over underlying array
+  for (int offset = blockIdx.x * blockDim.x; offset < t.size;
+       offset += gridDim.x * blockDim.x) {
+    int index = offset + threadIdx.x;
+    int r = curand(&state) % (b - a) + a;
+    if (index < t.size)
+      t(index) = r;
+  }
+}
+
+template void randu(Tensor<int, 2> t, int a, int b, long seed);
--- csrc/rand.cuh
+++ csrc/rand.cuh
@@ -5,3 +5,5 @@ Tensor<float, D> randn(shape_t<D> shape, float shift, float scale, long seed);
 
 template <uint D>
 void randn(Tensor<float, D> t, float shift, float scale, long seed);
+
+template <uint D> void randu(Tensor<int, D> t, int a, int b, long seed);
