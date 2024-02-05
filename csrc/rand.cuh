f642c77293ea3851b086f212c028d22157f5ba1a
skrider <skrider@berkeley.edu>
2024-02-05 00:30:15 +0000

    revert

--- csrc/rand.cuh
+++
@@ -1,9 +0,0 @@
-#include "tensor.cuh"
-
-template <uint D>
-Tensor<float, D> randn(shape_t<D> shape, float shift, float scale, long seed);
-
-template <uint D>
-void randn(Tensor<float, D> t, float shift, float scale, long seed);
-
-template <uint D> void randu(Tensor<int, D> t, int a, int b, long seed);
--- csrc/tensor_utils.cuh
+++
@@ -1,4 +0,0 @@
-#include "tensor.cuh"
-
-template <typename T, uint D>
-bool allclose(Tensor<T, D> a, Tensor<T, D> b, T eps);
